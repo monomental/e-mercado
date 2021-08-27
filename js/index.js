3dc7eff5ff843ad2622d7bc1f3c602e387bb4527
monomental <ivan.correa.bonora@gmail.com>
2021-08-26 23:41:36 -0300

    Ajuste login usuario y redireccionamiento a login

---
+++ js/index.js
@@ -0,0 +1,40 @@
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    var btnLogin = document.getElementById('btnLogin');
+    let infoMessage = document.getElementById('infoMessage');
+
+    btnLogin.addEventListener('click', function (e) {
+
+        let inputEmail = document.getElementById('inputEmail');
+        let inputPassword = document.getElementById('inputPassword');
+        let errorMessage = document.getElementById('errorMessage');
+        
+        function camposCompletos() {
+            return inputEmail.value && inputPassword.value;
+        }
+
+        if (camposCompletos()) {
+            localStorage.setItem('User-Logged', JSON.stringify({ email: inputEmail.value }));
+            window.location = 'home.html';
+
+        } else {
+            errorMessage.classList.remove('d-none');
+            errorMessage.classList.add('d-block');
+
+            errorMessage.innerText = "Debe ingresar los datos!";
+        }
+    });
+
+    if(localStorage.getItem('Logout')){
+        infoMessage.classList.remove('d-none');
+        infoMessage.classList.add('d-block');
+
+        infoMessage.innerText = 'Sesión cerrada'
+
+        localStorage.removeItem('Logout');
+    }else if(localStorage.getItem('User-Logged')){
+        window.location = 'home.html';
+    }
+});
--- js/init.js
+++ js/init.js
@@ -7,41 +7,66 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
-var showSpinner = function(){
+var showSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "block";
 }
 
-var hideSpinner = function(){
+var hideSpinner = function () {
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
-    var result = {};
-    showSpinner();
-    return fetch(url)
+var getJSONData = function (url) {
+  var result = {};
+  showSpinner();
+  return fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
-      }else{
+      } else {
         throw Error(response.statusText);
       }
     })
-    .then(function(response) {
-          result.status = 'ok';
-          result.data = response;
-          hideSpinner();
-          return result;
+    .then(function (response) {
+      result.status = 'ok';
+      result.data = response;
+      hideSpinner();
+      return result;
     })
-    .catch(function(error) {
-        result.status = 'error';
-        result.data = error;
-        hideSpinner();
-        return result;
+    .catch(function (error) {
+      result.status = 'error';
+      result.data = error;
+      hideSpinner();
+      return result;
     });
 }
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+document.addEventListener("DOMContentLoaded", function (e) {
+  let userLogged = localStorage.getItem('User-Logged');
+  let infoUser = document.getElementById("user-info");
+  let user = document.getElementById("user");
+  let btnSalir = document.getElementById('salir');
+
+  if (userLogged) {
+    userLogged = JSON.parse(userLogged);
+    user.innerText = user.innerText + 'Usuario logueado: ' + userLogged.email;
+    infoUser.classList.remove("d-none");
+    infoUser.classList.add('d-inline-block');
+
+  }
+  else if (infoUser) {
+    window.location = 'index.html';
+  }
+
+  if (btnSalir) {
+    btnSalir.addEventListener('click', function () {
+      localStorage.removeItem('User-Logged');
+      localStorage.setItem('Logout', 'logout');
+
+      window.location = 'index.html';
+    })
+  }
+
 });
